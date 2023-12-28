c873ae89a442a9bb01c52736fcbd248d025479bf
Jicheng Zhi <jzhi0004@student.monash.edu>
2023-12-28 23:02:02 +1100

    refactor: replace `addfinalizer()` with `yield` and use `@pytest_asyncio.fixture` for async fixtures

--- tests/opcuax/mock/test_client.py
+++ tests/opcuax/mock/test_client.py
@@ -5,7 +5,7 @@ from opcuax.types import BaseOpcuaClient
 
 @pytest.mark.asyncio
 async def test_context_variables_are_set(opcua_printer1):
-    printer = await opcua_printer1
+    printer = opcua_printer1
     assert printer.__client__ is not None
     assert isinstance(printer.__client__, BaseOpcuaClient)
     assert printer.__ns__ == 1
@@ -13,35 +13,30 @@ async def test_context_variables_are_set(opcua_printer1):
 
 @pytest.mark.asyncio
 async def test_get_default_value(opcua_client):
-    client = await opcua_client
     namespace = "ns=1;s=Printer_Name"
     default_value = "unknown"
 
-    value = await client.get(namespace, default=default_value)
+    value = await opcua_client.get(namespace, default=default_value)
 
     assert value == default_value
-    assert client.table[namespace] == default_value
+    assert opcua_client.table[namespace] == default_value
 
 
 @pytest.mark.asyncio
 async def test_get(opcua_client):
-    client = await opcua_client
-
     namespace = "ns=1;s=Printer_Name"
     value = "foobar"
 
-    client.table[namespace] = value
+    opcua_client.table[namespace] = value
 
-    assert (await client.get(namespace)) == value
+    assert (await opcua_client.get(namespace)) == value
 
 
 @pytest.mark.asyncio
 async def test_set(opcua_client):
-    client = await opcua_client
-
     namespace = "ns=1;s=Printer_Name"
     value = "foobar"
 
-    await client.set(namespace, value)
+    await opcua_client.set(namespace, value)
 
-    assert client.table[namespace] == value
+    assert opcua_client.table[namespace] == value
