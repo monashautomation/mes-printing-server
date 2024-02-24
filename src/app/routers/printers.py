2a5f28804bea0e8acffb97433691437a9e7112d5
Jicheng Zhi <jzhi0004@student.monash.edu>
2024-02-24 17:53:28 +1100

    refactor: use JSON as playload for `add_printer` API

--- src/app/routers/printers.py
+++ src/app/routers/printers.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 from typing import Annotated
 
 from fastapi import APIRouter, Form, HTTPException
-from pydantic import HttpUrl
+from pydantic import BaseModel, Field, HttpUrl
 from typing_extensions import TypedDict
 
 from app.dependencies import ctx
@@ -23,28 +23,26 @@ class PrinterId(TypedDict):
     id: int
 
 
+class CreatePrinter(BaseModel):
+    url: HttpUrl = Field(title="URL of the printer", examples=["http://localhost:5000"])
+    api_key: str = Field(title="API key", examples=["79ED0684040E4B96A34C3ABF4EA0A96A"])
+    opcua_name: str = Field(
+        title="browse name of the OPC UA printer object",
+        min_length=1,
+        examples=["Printer1"],
+    )
+    api: PrinterApi = Field(
+        title="Printer API", examples=["OctoPrint", "Prusa", "Mock"]
+    )
+
+
 @router.post("")
-async def add_printer(
-    url: Annotated[
-        HttpUrl, Form(title="URL of the printer", examples=["http://localhost:5000"])
-    ],
-    api_key: Annotated[
-        str, Form(title="API key", examples=["79ED0684040E4B96A34C3ABF4EA0A96A"])
-    ],
-    opcua_name: Annotated[
-        str,
-        Form(
-            title="browse name of the OPC UA printer object",
-            min_length=1,
-            examples=["Printer1"],
-        ),
-    ],
-    api: Annotated[
-        PrinterApi, Form(title="Printer API", examples=["OctoPrint", "Prusa", "Mock"])
-    ],
-) -> PrinterId:
+async def add_printer(model: CreatePrinter) -> PrinterId:
     printer = Printer(
-        url=str(url).rstrip("/"), api_key=api_key, opcua_name=opcua_name, api=api
+        url=str(model.url).rstrip("/"),
+        api_key=model.api_key,
+        opcua_name=model.opcua_name,
+        api=model.api,
     )
     async with ctx.database.new_session() as session:
         await session.upsert(printer)
